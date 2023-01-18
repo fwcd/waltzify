fb699b60dc1926d1b4f24cda374a23b34919fe29
fwcd <fwcdmail@gmail.com>
2023-01-18 13:44:00 +0100

    Read and write audio

--- waltzify/__init__.py
+++ waltzify/__init__.py
@@ -1,2 +1,14 @@
+from pathlib import Path
+from pydub import AudioSegment
+
+import argparse
+
 def main():
-    print('Hello')
+    parser = argparse.ArgumentParser(description='Converts a 4/4 song to 3/4')
+    parser.add_argument('--output', '-o', required=True, type=Path, help='The output (audio) file path.')
+    parser.add_argument('input', type=Path, help='The input (audio) file path.')
+
+    args = parser.parse_args()
+
+    audio = AudioSegment.from_file(args.input)
+    audio.export(args.output)
